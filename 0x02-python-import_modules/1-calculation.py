1adc1abf74384ab3c6247aa8e8a6dea8b8fc1f36
Ken-Obieze <kenneth.obieze@outlook.com>
2022-09-22 23:48:06 +0100

    second

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == main:
-    from calculator_1 import add, sub, mul, div
     """function prints result of deveral arithmetic operations"""
+    from calculator_1 import add, sub, mul, div 
     a = 10
     b = 5
-
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
