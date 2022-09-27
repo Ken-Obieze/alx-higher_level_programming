b9f0b61c3c93891ac75dd20df794b1ba3f15121e
Ken-Obieze <kenneth.obieze@outlook.com>
2022-09-27 05:56:20 +0100

    seventh

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
     for i in  range(2):
-        if tuple_a[i] < 0 or tuple_a[i] == '':
+        if tuple_a[i] < 0 or tuple_a[i] == ' ':
             tuple_a[i] = 0
-        if tuple_b[i] < 0 or tuple_b[i] == '':
+        if tuple_b[i] < 0 or tuple_b[i] == ' ':
             tuple_b[i] = 0
         new_tuple[i] = tuple_a[i] + tuple_b[i]
     return (new_tuple)
