750fe507db95bd1a120bcac757439566831afdd1
Ken-Obieze <kenneth.obieze@outlook.com>
2022-09-29 04:01:29 +0100

    bulk

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = [:]
     for i in range(0, len(new_list) - 1):
-        if i == search:
+        if i is search:
             new_list[i] = replace
     return (new_list)
