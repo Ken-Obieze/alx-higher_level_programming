3c1509afffcba312b0c9c471456e81ee28ce53d0
Ken-Obieze <kenneth.obieze@outlook.com>
2022-10-13 05:20:32 +0100

    first

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idx, count  = 0, 0
+    while idx < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        idx += 1
+        print()
+        return (count)
