50ab5f849ef6af43359b78a21d40142ecf8dcc99
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-21 22:25:10 +0100

    fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     """ checks if numbers in list are even"""
     evenCheck = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             evenCheck.append(True)
         else:
             evenCheck.append(False)
