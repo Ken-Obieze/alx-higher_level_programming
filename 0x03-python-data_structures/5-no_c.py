7f97e7fcab89b08122013a96629746850e14857a
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-21 16:08:23 +0100

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    """
-    function that removes c from strings
-    """
+    """ function that removes c from strings """
     return (my_string.translate({ord(char): None for char in "cC"}))
