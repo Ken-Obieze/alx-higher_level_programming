70022929c6f808d0380d745f1a090fd8c0c33f4b
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-22 15:59:53 +0100

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while count is not x:
             print(my_list[count], end='')
             count += 1
-    except:
+    except IndexError:
         None
     print()
     return (count)
