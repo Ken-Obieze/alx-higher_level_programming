3b94dd258c463a437414fe071f0210ae28d46f6c
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-23 10:46:44 +0100

    fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,4 @@ def safe_print_integer_err(value):
         return(True)
     except (ValueError, TypeError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        print()
         return (False)
