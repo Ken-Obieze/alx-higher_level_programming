9f5cdaa6e1fa93c620f54e71dddb776ae70f9cbf
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-23 10:20:02 +0100

    fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    i, result = 0, 0;
+    i, result = 0, 0
     while i < list_length:
         try:
             result = my_list_1[i] / my_list_2[i]
