c15c98b37053a276e045df40a4432ff3ee755545
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-25 11:25:12 +0100

    4-square implementation

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Defines Square"""
+
+
+class Square:
+    """ represents square """
+
+    def __init__(self, size=0):
+        """ initializes new square.
+
+        Args:
+            size: size of square
+
+        Raises:
+            TypeError: if type is not an int
+            ValueError: if value is less than 0
+        """
+        self.size = size
+    @property
+    def size(self):
+        """ Gets current value of size"""
+        return (size.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ returns area of tsquare """
+        return (self.__size ** 2)
