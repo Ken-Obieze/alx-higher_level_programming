55b18691d953f87626162eb2bef948ef089f2931
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-26 21:16:35 +0100

    103-square implementation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -15,7 +15,6 @@ class MagicClass:
         Raises:
             TypeError: when radius isn't int or float
         """
-        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
