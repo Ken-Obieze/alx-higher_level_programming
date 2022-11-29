9652a095d6f6c88f1097876b05bd25de04ad5ed0
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-29 23:36:00 +0100

    Task 0 TDD fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b = 98):
     if (type(a) != int or type(b) != int and 
             type(a) != float or type(b) != float):
         if (type(a) != int):
-            raise TypeError(a must be an integer)
+            raise TypeError("a must be an integer")
         if (type(b) != int):
-            raise TypeError(b must be an integer)
+            raise TypeError("b must be an integer")
     else:
         if (type(a) == float):
             a = int(a)
