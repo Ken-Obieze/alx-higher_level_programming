06f6b6673c912cb02bcfdcc4b9c760f46076c8ee
Ken-Obieze <kenneth.obieze@outlook.com>
2022-11-30 14:33:12 +0100

    Bulk-commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Locked class Module"""
+
+
+class LockedClass:
+    """Represents a class with restricted attribute modification."""
+    __slots__ = ['first_name']
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Module implements copy_list(l) function """
+
+
+def copy_list(l):
+    return l.copy()
