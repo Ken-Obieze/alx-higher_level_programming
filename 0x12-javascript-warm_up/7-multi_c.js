e44544a7def44ac7f07dacb820e9f66fcbbfe4df
Ken-Obieze <kenneth.obieze@outlook.com>
2022-12-19 22:12:44 +0100

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2]);
 if (!isNaN(parseInt(process.argv[2]))) {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < num; i++) {
     console.log('Cis fun');
   }
 } else {
