a6026f89f446271799359a9f0739ca30630fa6c0
Ken-Obieze <kenneth.obieze@outlook.com>
2022-12-19 22:18:26 +0100

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-
+const x = parseInt(process.argv[2]);
+if (!isNaN(parseInt(process.argv[2]))) {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      console.log('X');
+    }
+  }
+} else {
+  console.log('Missing size');
+}
