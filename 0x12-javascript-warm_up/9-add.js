d88efe207f5482699c2e8df42b2404b0cf4bf84d
Ken-Obieze <kenneth.obieze@outlook.com>
2022-12-19 22:44:13 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-  console.log(parseInt(process.argv[2]) + parseInt(process.argv[2]));
+  console.log(parseInt(a) + parseInt(b));
 }
+
+add(process.argv[2], process.argv[3]);
