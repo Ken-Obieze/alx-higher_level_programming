72515531fbf2e89c7a19da50152fee3098c14ec1
Ken-Obieze <kenneth.obieze@outlook.com>
2022-12-28 03:09:34 +0100

    task 0 fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """Module for selecting states in database."""
 
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
 
-    if len(argv) >= 4:
-        con = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1],
-	    password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute('SELECT * FROM states ORDER BY id ASC;')
-    db = cursor.fetchall()
-    for i in db:
-    	print(i)
-    cursor.close()
-    con.close()
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        db_connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cursor = db_connection.cursor()
+        cursor.execute('SELECT * FROM states ORDER BY id ASC;')
+        results = cursor.fetchall()
+        for result in results:
+            print(result)
+        db_connection.close()
