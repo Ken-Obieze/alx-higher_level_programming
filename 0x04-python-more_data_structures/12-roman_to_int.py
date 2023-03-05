6b6672c16b4e9ca001925fefae15551355e17546
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-05 03:54:46 +0100

    task-12--final-fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,13 @@
 def roman_to_int(roman_string):
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'c': 100, 'D': 500, 'M': 1000}
     int_coversion = 0
+    last_digit = 0
     roman_list = list(roman_string)
-    for i, c  in enumerate(roman_list):
-        if (i+1) == len(roman_string) or roman_dict[c] >= roman_dict[roman_list[i+1]]::
-            int_conversion += roman_dict[c]
+    for i in roman_list[::-1]:
+        digit = roman_dict[i]
+        if digit >= last_digit:
+            int_conversion += digit
+            last_digit = digit
         else:
-            int_conversion -= roman_dict[c]
+            int_conversion -= digit
     return(int_conversion)
