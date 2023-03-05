250a727dd7d43266c0b46bbaa1d0e4450bfecd27
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-05 03:16:24 +0100

    task-12-fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,12 @@ def roman_to_int(roman_string):
         return (0)
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'c': 100, 'D': 500, 'M': 1000}
     int_coversion = 0
-    for i in range(len(roman_string)):
+    roman_list = list(roman_string)
+    j = 0
+    for i in roman_list:
         if i > 0:
-            int_conversion += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+            int_conversion += roman_dict[roman_list[j]] - 2 * roman_dict[roman_list[j - 1]]
         else:
-            int_conversion = roman_dict[roman_string[i]]
+            int_conversion = roman_dict[roman_list[j]]
+        j += 1
     return(int_conversion)
