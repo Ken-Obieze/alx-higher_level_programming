4c5c86416e8234e82f0d53099e03191a1d510880
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-05 03:58:10 +0100

    task-12--final-fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'c': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_conversion = 0
     last_digit = 0
     roman_list = list(roman_string)
