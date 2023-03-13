0c780d6dc252b3df0f5fba9acb83c5f9c95ee50e
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-13 21:18:11 +0100

    tasks_0-7_fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-Module implement a scriptthat adds all args to a list.
-"""
+"""Module implement a scriptthat adds all args to a list."""
 import sys
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
-load_firom_json_file = __import__("6-load_from_json_file.py").load_from_json_file:
+import json
+
 
 if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+    load_firom_json_file = __import__("6-load_from_json_file.py").load_from_json_file
     contents = load_from_json_file("add_item.json")
-    contents.extend(sys.argv[1:])
-    save_to_json_file(contnts, "add_item.json")
+    for i in range(1, len(sys.argv)):
+        contents.append(sys.argv[i])
+    save_to_json_file(contents, "add_item.json")
