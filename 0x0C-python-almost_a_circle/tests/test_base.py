ac72b63f91c1973d8165ea04d31a1ce6f5a2e72e
Ken-Obieze <kenneth.obieze@outlook.com>
2023-03-28 13:06:28 +0100

    test fix1c

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -35,5 +35,43 @@ class TestBase_instatntiotion(unittest.TestCase):
         self.assertEqual(t1.id, 89)
 
 
+class TestBase_to_json(unittest.TestCase):
+    """test for json string."""
+
+    def test_base_to_json_none(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_base_to_json_emptylist(self):
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_base_to_json_specifics_reloads(self):
+        d = [{'x': 11, 'y': 23, 'width': 321, 'id': 54,
+             'height': 30}]
+        self.assertEqual(len(Base.to_json_string(d)),
+                         len(str(d)))
+        d = [{'x': 1, 'y': 2, 'width': 3, 'id': 4, 'height': 5}]
+        self.assertEqual(len(Base.to_json_string(d)),
+                         len(str(d)))
+        d = [{"froth": 988}]
+        self.assertEqual(Base.to_json_string(d),
+                         '[{"froth": 988}]')
+
+        d = [{"froth": 988}, {"abc": 123}, {"HI": 0}]
+        self.assertEqual(Base.to_json_string(d),
+                         '[{"froth": 988}, {"abc": 123}, {"HI": 0}]')
+
+        d = [{'x': 1, 'y': 2, 'width': 3, 'id': 4, 'height': 5},
+             {'x': 100, 'y': 123, 'width': 321, 'id': 543,
+             'height': 34340}]
+        self.assertEqual(len(Base.to_json_string(d)),
+                         len(str(d)))
+        d = [{}]
+        self.assertEqual(Base.to_json_string(d),
+                         '[{}]')
+        d = [{}, {}]
+        self.assertEqual(Base.to_json_string(d),
+                         '[{}, {}]')
+
+
 if __name__ == "__main__":
     unittest.main()
